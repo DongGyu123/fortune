47e90071161382d32292155d2f2f3a28ae5c4bae
20225087 <163315474+20225087@users.noreply.github.com>
2024-06-06 21:17:43 +0900

    Update today_fortune.py

--- Frontend/functions/today_fortune.py
+++ Frontend/functions/today_fortune.py
@@ -7,8 +7,6 @@ def execute(
     key: str,
     model: InputModel,
 ) -> OutputModel | None:
-    real_val=model
-    print("Frontend/functions/today_fortune.py: \n", real_val)
     return page.settings.client.call(
         function=page.function,
         input=real_val,
